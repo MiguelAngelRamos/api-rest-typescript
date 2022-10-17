603b01cd0dc8d0e0bb660ad2e950dfa3672cd2b4
Miguel Ramos <return.infox@gmail.com>
2022-10-17 20:04:31 -0300

    app ts configurado

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development'; //* Si esta difinido utilize la variable de entorno en caso que no por defecto 'development'
+
 import dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config({
+  path: `${__dirname}/../config/${process.env.APP_ENV}.env`
+});
+
+const app: express.Application = express();
+
+export { app };
