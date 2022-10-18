5480ff990a1fa26e3a7440730ba5c56b00ffd20f
Miguel Ramos <return.infox@gmail.com>
2022-10-17 21:33:56 -0300

    creacion del entity subcription

---
+++ src/services/repositories/domain/ISubscriptionDomain.ts
@@ -0,0 +1,9 @@
+export interface ISubscriptionDomain {
+  id: number;
+  user_id: number;
+  code: string;
+  amount: number;
+  cron: string;
+  created_at: Date | null,
+  updated_at: Date | null
+}
