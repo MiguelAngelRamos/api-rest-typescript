263046d365e3f3052da7ccb4598f37c9d698d02a
Miguel Ramos <return.infox@gmail.com>
2022-10-18 19:00:35 -0300

    Subscription Repository Interface

--- src/services/repositories/domain/ISubscriptionDomain.ts
+++ src/services/repositories/domain/ISubscriptionDomain.ts
@@ -4,6 +4,26 @@ export interface ISubscriptionDomain {
   code: string;
   amount: number;
   cron: string;
-  created_at: Date | null,
-  updated_at: Date | null
+  created_at: Date | null;
+  updated_at: Date | null;
 }
+
+// export class SubcriptionDomain {
+//   public id: number;
+//   public user_id: number;
+//   public code: string;
+//   public amount: number;
+//   public cron: string;
+//   public created_at: Date | null;
+//   public updated_at: Date | null;
+
+//   constructor(id: number, user_id: number, code: string, amount: number, cron: string, created_at: Date, updated_at: Date) {
+//      this.id = id;
+//      this.user_id = user_id;
+//      this.code = code;
+//      this.amount = amount;
+//      this.cron = cron;
+//      this.created_at = created_at;
+//      this.updated_at = updated_at;
+//   }
+// }
---
+++ src/services/repositories/interfaces/ISubscriptionRepository.ts
@@ -0,0 +1,17 @@
+import { ISubscriptionDomain } from "../domain/ISubscriptionDomain";
+
+export interface ISubscriptionRepository {
+
+  all(): Promise<ISubscriptionDomain[]>;
+
+  find(id: number): Promise<ISubscriptionDomain | null>;
+
+  findByUserIdAndCode(user_id: number, code: string): Promise<ISubscriptionDomain | null>;
+
+  store(entry: ISubscriptionDomain): Promise<void>;
+
+  update(entry: ISubscriptionDomain): Promise<void>;
+
+  remove(id: number): Promise<void>
+
+}
