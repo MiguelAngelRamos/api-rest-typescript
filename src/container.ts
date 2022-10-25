ebdea98b5163f195d617ad99008bf6d7a946c566
Miguel Ramos <return.infox@gmail.com>
2022-10-25 19:15:00 -0300

    correcion de container

--- src/container.ts
+++ src/container.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import { createContainer, asClass } from 'awilix';
 import { scopePerRequest } from 'awilix-express';
 // import { SubscriptionMySQLRepository } from './services/repositories/impl/mysql/subscription.repository';
+import { BalanceMSSQLRepository } from './services/repositories/impl/mssql/balance.repository';
+import { MovementMSSQLRepository } from './services/repositories/impl/mssql/movement.repository';
+import { MovementService } from './services/movement.service';
+// import { BalanceMysqlRepository } from './services/repositories/impl/mysql/balance.repository';
+// import { MovementMySQLRepository } from './services/repositories/impl/mysql/movement.repository';
 import { SubscriptionService } from './services/subscription.service';
 // import { SubscriptionMSSQLRepository } from './services/repositories/impl/mssql/subscription.respository';
 import { SubscriptionMySQLRepository } from './services/repositories/impl/mysql/subscription.repository';
@@ -15,8 +20,13 @@ export default (app: express.Application) => {
       //* Repositories
       subscriptionRepository: asClass(SubscriptionMySQLRepository).scoped(), 
       // subscriptionRepository: asClass(SubscriptionMSSQLRepository).scoped(), 
+      // movementRepository: asClass(MovementMySQLRepository).scoped(),
+      // balanceRepository: asClass(BalanceMysqlRepository).scoped,
+      movementRepository: asClass(MovementMSSQLRepository).scoped(),
+      balanceRepository: asClass(BalanceMSSQLRepository).scoped(),
       //* Servicios
-      subscriptionService: asClass(SubscriptionService).scoped()
+      subscriptionService: asClass(SubscriptionService).scoped(),
+      movementService: asClass(MovementService).scoped()
     }
   );
   app.use(scopePerRequest(container));
